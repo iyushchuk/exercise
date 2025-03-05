203883c967c42e5ab24b9cd5a224387043e136cc
Ivan Yushchuk <yuschuk.ivan@gmail.com>
2025-03-05 14:29:16 +0100

    Project initialisation

---
+++ src/test/kotlin/com/deblock/exercise/ExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deblock.exercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExerciseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
