cf5b0ddd5d271665efe93effb840b6345d7b92d6
Ivan Yushchuk <yuschuk.ivan@gmail.com>
2025-03-07 11:09:50 +0100

    Cleanup

--- src/main/kotlin/com/deblock/exercise/flights/FlightsRequest.kt
+++ src/main/kotlin/com/deblock/exercise/flights/FlightsRequest.kt
@@ -8,18 +8,18 @@ import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
 
 data class FlightsRequest(
-    @field:NotNull(message = "Hello") @field:Pattern(regexp = "^([A-Za-z]{3}+)\$")
+    @field:NotNull @field:Pattern(regexp = "^([A-Za-z]{3}+)\$")
     val origin: String,
 
-    @field:NotNull(message = "Hello") @field:Pattern(regexp = "^([A-Za-z]{3}+)\$")
+    @field:NotNull @field:Pattern(regexp = "^([A-Za-z]{3}+)\$")
     val destination: String,
 
-    @field:NotNull(message = "Hello") @field:DateTimeFormat( pattern = "yyyy-MM-dd")
+    @field:NotNull @field:DateTimeFormat( pattern = "yyyy-MM-dd")
     val departureDate: LocalDate,
 
-    @field:NotNull(message = "Hello") @field:DateTimeFormat( pattern = "yyyy-MM-dd")
+    @field:NotNull @field:DateTimeFormat( pattern = "yyyy-MM-dd")
     val returnDate: LocalDate,
 
-    @field:NotNull(message = "Hello") @field:Min(1) @field:Max(4)
+    @field:NotNull @field:Min(1) @field:Max(4)
     val numberOfPassengers: Int
 )
